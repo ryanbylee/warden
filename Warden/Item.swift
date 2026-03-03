c53102ad51093b4dbbb3f186ebab45ca158c310c
Ryan Lee <ryanbylee@ucla.edu>
2026-03-02 18:18:17 -0800

    Initial Commit

---
+++ Warden/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Warden
+//
+//  Created by Ryan Lee on 3/2/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
